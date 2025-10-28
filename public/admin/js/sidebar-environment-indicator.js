a555ebf344cc68333220b2a32063577ec43807e8
torqdev <67965755+torqdev@users.noreply.github.com>
2025-10-28 13:50:17 -0300

    Merge pull request #27 from TorqIT/bugfix/sidebar-panel-collapse
    Fix sidebar collapse bug

--- public/admin/js/sidebar-environment-indicator.js
+++ public/admin/js/sidebar-environment-indicator.js
@@ -1,9 +1,9 @@
 document.addEventListener(pimcore.events.pimcoreReady, () => {
     Ext.get("pimcore_navigation").insertHtml(
       "beforeEnd",
-      '<h1 style="transform: rotate(-90deg); margin-left: 20px; margin-top: 200px; color: #d7d7d7; transform-origin:0 0">' +
+      '<h1 style="position: absolute;transform: rotate(-90deg); margin-left: 20px; margin-top: 200px; color: #d7d7d7; transform-origin:0 0">' +
         (pimcore?.settings?.environment || "").toUpperCase() +
-        "</h1>"
+      '</h1>'
     );
-  });
+});
   
