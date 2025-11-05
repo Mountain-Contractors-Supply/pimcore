f40e32b3eb409a65ed6a2f7bdf309e91d2f6bcbc
Evan Jackson <evan.jackson@torqit.ca>
2025-11-05 11:16:09 -0500

    Add configuration of default composer directories to get rid of warnings

--- .docker/composer-install-dependencies.sh
+++ .docker/composer-install-dependencies.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo Configuring Composer...
+mkdir -p /var/www/.cache/composer && chown -R www-data:www-data /var/www/.cache/composer
+mkdir -p /var/www/.config/composer && chown -R www-data:www-data /var/www/.config/composer
 echo Installing Composer packages...
 cd /var/www/html && runuser -u www-data -- php -d memory_limit=-1 -d xdebug.remote_enable=0 /usr/local/bin/composer install --prefer-dist --no-scripts
