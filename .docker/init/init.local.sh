566ddde31a1bb0d94022e1b477040ea98197c04b
Evan Jackson <evan.jackson@torqit.ca>
2026-03-18 09:56:25 -0400

    Fix secret parsing in init

--- .docker/init/init.local.sh
+++ .docker/init/init.local.sh
@@ -6,7 +6,7 @@ source /etc/profile.d/secrets-to-env-vars.sh
 
 /composer-install-dependencies.sh
 
-if [ "$(mysql -h "$DATABASE_HOST" -u "$DATABASE_USER" -p"$DATABASE_PASSWORD" \
+if [ "$(mysql -h "$DATABASE_HOST" -u "$DATABASE_USER" -p"$(cat /run/secrets/database-password)" \
       -sse "select count(*) from information_schema.tables where table_schema='pimcore' and table_name='assets';")" -ne 0 ]
 then
     # Only run cache clear if the database is seeded. If it is not, trying to clear the cache will cause errors.
--- .docker/init/init.sh
+++ .docker/init/init.sh
@@ -4,23 +4,22 @@ set -e
 
 source /etc/profile.d/secrets-to-env-vars.sh
 
-if [ "$(mysql -h "$DATABASE_HOST" -u "$DATABASE_USER" -p"$DATABASE_PASSWORD" \
+if [ "$(mysql -h "$DATABASE_HOST" -u "$DATABASE_USER" -p"$(cat /run/secrets/database-password)" \
       -sse "select count(*) from information_schema.tables where table_schema='pimcore' and table_name='assets';")" -eq 0 ] \
    && [ "$PIMCORE_INSTALL" = "true" ]
 then
   echo "Database is empty and PIMCORE_INSTALL is set to true, so calling pimcore-install..."
-  PIMCORE_INSTALL_ENCRYPTION_SECRET=$PIMCORE_ENCRYPTION_SECRET \
-  PIMCORE_INSTALL_INSTANCE_IDENTIFIER=$PIMCORE_INSTANCE_IDENTIFIER \
-  PIMCORE_INSTALL_PRODUCT_KEY=$PIMCORE_PRODUCT_KEY \  
-  PIMCORE_INSTALL_MYSQL_HOST_SOCKET=$DATABASE_HOST \
-  PIMCORE_INSTALL_MYSQL_PORT=$DATABASE_PORT \
-  PIMCORE_INSTALL_MYSQL_USERNAME=$DATABASE_USER \
-  PIMCORE_INSTALL_MYSQL_DATABASE=$DATABASE_NAME \
-  PIMCORE_INSTALL_ADMIN_USERNAME=admin \
-  PIMCORE_INSTALL_MYSQL_PASSWORD=$(cat /run/secrets/database-password) \
-  PIMCORE_INSTALL_ADMIN_PASSWORD=$(cat /run/secrets/pimcore-admin-password) \
-
-    runuser -u www-data -- vendor/bin/pimcore-install --skip-database-config --no-interaction
+  export PIMCORE_INSTALL_ENCRYPTION_SECRET=$PIMCORE_ENCRYPTION_SECRET
+  export PIMCORE_INSTALL_INSTANCE_IDENTIFIER=$PIMCORE_INSTANCE_IDENTIFIER
+  export PIMCORE_INSTALL_PRODUCT_KEY=$PIMCORE_PRODUCT_KEY
+  export PIMCORE_INSTALL_MYSQL_HOST_SOCKET=$DATABASE_HOST
+  export PIMCORE_INSTALL_MYSQL_PORT=$DATABASE_PORT
+  export PIMCORE_INSTALL_MYSQL_USERNAME=$DATABASE_USER
+  export PIMCORE_INSTALL_MYSQL_DATABASE=$DATABASE_NAME
+  export PIMCORE_INSTALL_ADMIN_USERNAME=admin
+  export PIMCORE_INSTALL_MYSQL_PASSWORD=$(cat /run/secrets/database-password)
+  export PIMCORE_INSTALL_ADMIN_PASSWORD=$(cat /run/secrets/pimcore-admin-password)
+  runuser -u www-data -- vendor/bin/pimcore-install --skip-database-config --no-interaction
 fi
 
 echo Installing bundles...
@@ -37,6 +36,17 @@ runuser -u www-data -- /var/www/html/bin/console torq:folder-creator
 
 echo Generating roles...
 runuser -u www-data -- /var/www/html/bin/console torq:generate-roles
+
+echo Rebuilding class definitions for indexes...
+if [ ! -f /var/www/html/var/installer/.index-complete ]; then
+  echo "Fresh install detected, running full index rebuild..."
+  runuser -u www-data -- /var/www/html/bin/console generic-data-index:update:index -r
+  touch /var/www/html/var/installer/.index-complete
+else
+  runuser -u www-data -- /var/www/html/bin/console generic-data-index:update:index --update-global-aliases-only
+  runuser -u www-data -- /var/www/html/bin/console generic-data-index:deployment:reindex
+  runuser -u www-data -- /var/www/html/bin/console generic-data-index:reindex
+fi
 
 echo Clearing Pimcore cache...
 runuser -u www-data -- /var/www/html/bin/console pimcore:cache:clear
