b5ee4c4c21725e724556f14dfcfe4a22cddcf3d0
Evan Jackson <evan.jackson@torqit.ca>
2026-03-18 09:56:43 -0400

    Add APP_SECRET to secrets-to-env-vars for Studio

--- .docker/secrets-to-env-vars.sh
+++ .docker/secrets-to-env-vars.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Certain secrets cause problems with Pimcore when used as files - this script converts them to env vars, and then containers can source this script at startup.
+# Certain secrets cause problems with Symfony and Pimcore when used as files - this script converts them to env vars, and then containers can source this script at startup.
 # IMPORTANT: Only use this for secrets that must be converted to env vars. For all other secrets, continue to use them as files.
 export PIMCORE_ENCRYPTION_SECRET=$(cat /run/secrets/pimcore-encryption-secret 2>/dev/null || echo "")
 export PIMCORE_INSTANCE_IDENTIFIER=$(cat /run/secrets/pimcore-instance-identifier 2>/dev/null || echo "")
 export PIMCORE_PRODUCT_KEY=$(cat /run/secrets/pimcore-product-key 2>/dev/null || echo "")
+export APP_SECRET=$(cat /run/secrets/kernel-secret 2>/dev/null || echo "")
