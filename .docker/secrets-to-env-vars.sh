55a2957f67edbb35d999edc6f06fbe0f4107e11a
Logan MacKinnon <logan.mackinnon@torqit.ca>
2026-03-26 14:47:26 -0300

    Refactor secrets handling in environment variables script and services configuration

--- .docker/secrets-to-env-vars.sh
+++ .docker/secrets-to-env-vars.sh
@@ -5,3 +5,5 @@
 export PIMCORE_ENCRYPTION_SECRET=$(cat /run/secrets/pimcore-encryption-secret 2>/dev/null || echo "")
 export PIMCORE_INSTANCE_IDENTIFIER=$(cat /run/secrets/pimcore-instance-identifier 2>/dev/null || echo "")
 export PIMCORE_PRODUCT_KEY=$(cat /run/secrets/pimcore-product-key 2>/dev/null || echo "")
+export KERNEL_SECRET=$(cat /run/secrets/kernel-secret 2>/dev/null || echo "")
+export APP_SECRET=$(cat /run/secrets/kernel-secret 2>/dev/null || echo "")
