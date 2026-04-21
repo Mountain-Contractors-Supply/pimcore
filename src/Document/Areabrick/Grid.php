2ecb4e7e95bfc4f3042c58ef490a5c7eebc1c793
Khanon Curtis <khanon.curtis@mountainland.com>
2026-04-21 10:24:11 -0600

    feat: wire Grid areabrick to CvaComponent with cols/gap variants

--- src/Document/Areabrick/Grid.php
+++ src/Document/Areabrick/Grid.php
@@ -5,23 +5,25 @@ declare(strict_types=1);
 namespace App\Document\Areabrick;
 
 use Pimcore\Extension\Document\Areabrick\Attribute\AsAreabrick;
-use Pimcore\Extension\Document\Areabrick\AbstractTemplateAreabrick;
 
 #[AsAreabrick(id: 'grid')]
-final class Grid extends AbstractTemplateAreabrick
+final class Grid extends AbstractConfigurableAreabrick
 {
+    #[\Override]
     public function getName(): string
     {
         return 'Grid';
     }
 
+    #[\Override]
     public function getDescription(): string
     {
-        return 'Grid';
+        return 'Configurable column grid';
     }
 
-    public function needsReload(): bool
+    #[\Override]
+    public function getComponentClassName(): string
     {
-        return false;
+        return \McSupply\EcommerceBundle\Twig\Components\Layout\Grid::class;
     }
 }
