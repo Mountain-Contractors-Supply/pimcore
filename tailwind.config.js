90ae9de25c983990d415e016acb1bc7adbb2d646
Doug Peterson <doug.peterson@mountainland.com>
2026-04-21 09:13:53 -0600

    Remove most style.css classes (#83)

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,6 @@ module.exports = {
     "./templates/**/*.html.twig",
     "./vendor/mcsupply/ecommerce-bundle/templates/**/*.html.twig",
     "./vendor/mcsupply/ecommerce-bundle/src/Twig/Components/**/*.php",
-    "./bundles/ecommerce-bundle/src/Twig/Components/**/*.php",
   ],
   safelist: [
     'lg:grid-cols-[1fr_3fr]',
